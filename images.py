47585fbb6f11c49b70426c165c9bee6d90c7a156
Bence Ollé <45627657+olledalow@users.noreply.github.com>
2019-06-03 20:14:34 +0200

    Updating the GUI version
    This version of the game is finally playable. Though this version still lacks some key functions (like: weapon upgrades, spell upgrades, more engaging combat, more monsters to fight with, etc...).
    In order to play: 
    - Windows: unzip the LAUNCHER.zip file, open the folder and run the launcher.exe file. note: python3 is required.
    -Windows/MacOS/Unix : download all the files, unzip the ADVENTURE_TEXT.zip and the GAME_IMAGES.zip files. run the GUI.py file in a Python IDLE (Pycharm is recommended).

--- images.py
+++ images.py
@@ -1,5 +1,108 @@
 import tkinter as tk
 
+in_meadow = tk.PhotoImage(file='GAME_IMAGES/landscape.png')
 in_village = tk.PhotoImage(file='GAME_IMAGES/in_village.png')
 trade = tk.PhotoImage(file='GAME_IMAGES/trade.png')
 rest = tk.PhotoImage(file='GAME_IMAGES/bed.png')
+bag = tk.PhotoImage(file='GAME_IMAGES/bag.png')
+in_bag = tk.PhotoImage(file='GAME_IMAGES/in_bag.png')
+companions = tk.PhotoImage(file='GAME_IMAGES/companions.png')
+companions_big = tk.PhotoImage(file='GAME_IMAGES/companions_big.png')
+villagers = tk.PhotoImage(file='GAME_IMAGES/villagers.png')
+in_ship = tk.PhotoImage(file='GAME_IMAGES/in_ship.png')
+crew = tk.PhotoImage(file='GAME_IMAGES/crew.png')
+in_jungle = tk.PhotoImage(file='GAME_IMAGES/in_jungle.png')
+in_thicket = tk.PhotoImage(file='GAME_IMAGES/in_thicket.png')
+in_altar = tk.PhotoImage(file='GAME_IMAGES/in_altar.png')
+in_sanctuary = tk.PhotoImage(file='GAME_IMAGES/in_sanctuary.png')
+in_cave = tk.PhotoImage(file='GAME_IMAGES/in_cave.png')
+in_pyramid = tk.PhotoImage(file='GAME_IMAGES/in_pyramid.png')
+in_lava = tk.PhotoImage(file="GAME_IMAGES/in_lava.png")
+bed = tk.PhotoImage(file="GAME_IMAGES/bed.png")
+
+
+#  CAVE ADVENTURE IMAGES
+cave_adventure1 = tk.PhotoImage(file="GAME_IMAGES/cave/cave_adventure1.png")
+
+cave_adventure2 = tk.PhotoImage(file="GAME_IMAGES/cave/cave_adventure2.png")
+cave_adventure2_lucky = tk.PhotoImage(file="GAME_IMAGES/cave/cave_adventure2_lucky.png")
+cave_adventure2_not_lucky = tk.PhotoImage(file="GAME_IMAGES/cave/cave_adventure2_lucky.png")
+
+
+cave_adventure3 = tk.PhotoImage(file="GAME_IMAGES/cave/cave_adventure3.png")
+cave_adventure3_lucky = tk.PhotoImage(file="GAME_IMAGES/cave/cave_adventure3_lucky.png")
+cave_adventure3_not_lucky = tk.PhotoImage(file="GAME_IMAGES/cave/cave_adventure3_lucky.png")
+
+
+cave_adventure4 = tk.PhotoImage(file="GAME_IMAGES/cave/cave_adventure4.png")
+cave_adventure4_lucky = tk.PhotoImage(file="GAME_IMAGES/cave/cave_adventure2_lucky.png")
+cave_adventure4_not_lucky = tk.PhotoImage(file="GAME_IMAGES/cave/cave_adventure4_not_lucky.png")
+
+
+cave_adventure5 = tk.PhotoImage(file="GAME_IMAGES/cave/cave_adventure5.png")
+cave_adventure5_lucky = tk.PhotoImage(file="GAME_IMAGES/cave/cave_adventure5_lucky.png")
+cave_adventure5_not_lucky = tk.PhotoImage(file="GAME_IMAGES/cave/cave_adventure5_lucky.png")
+
+
+#  SANCTUARY ADVENTURE IMAGES
+sanctuary_adventure1 = tk.PhotoImage(file="GAME_IMAGES/sanctuary/sanctuary_adventure1.png")
+
+sanctuary_adventure2 = tk.PhotoImage(file="GAME_IMAGES/sanctuary/sanctuary_adventure2.png")
+sanctuary_adventure2_lucky = tk.PhotoImage(file="GAME_IMAGES/sanctuary/sanctuary_adventure2_lucky.png")
+sanctuary_adventure2_catastrophe = tk.PhotoImage(file="GAME_IMAGES/sanctuary/sanctuary_adventure2_catastrophe.png")
+sanctuary_adventure2_curse_geyser = tk.PhotoImage(file="GAME_IMAGES/sanctuary/geyser_curse.png")
+sanctuary_adventure2_curse_volcano = tk.PhotoImage(file="GAME_IMAGES/sanctuary/volcano_curse.png")
+
+sanctuary_adventure3 = tk.PhotoImage(file="GAME_IMAGES/sanctuary/sanctuary_adventure3.png")
+sanctuary_adventure3_lucky = tk.PhotoImage(file="GAME_IMAGES/sanctuary/sanctuary_adventure3_lucky.png")
+sanctuary_adventure3_not_lucky = tk.PhotoImage(file="GAME_IMAGES/sanctuary/sanctuary_adventure3.png")
+
+
+sanctuary_adventure4 = tk.PhotoImage(file="GAME_IMAGES/sanctuary/sanctuary_adventure4.png")
+sanctuary_adventure4_lucky = tk.PhotoImage(file="GAME_IMAGES/sanctuary/sanctuary_adventure4.png")
+sanctuary_adventure4_not_lucky = tk.PhotoImage(file="GAME_IMAGES/sanctuary/sanctuary_adventure4.png")
+
+
+sanctuary_adventure5 = tk.PhotoImage(file="GAME_IMAGES/sanctuary/sanctuary_adventure5.png")
+sanctuary_adventure5_lucky = tk.PhotoImage(file="GAME_IMAGES/sanctuary/sanctuary_adventure5.png")
+sanctuary_adventure5_not_lucky = tk.PhotoImage(file="GAME_IMAGES/sanctuary/sanctuary_adventure5.png")
+
+
+#  ALTAR ADVENTURE IMAGES
+altar_adventure1 = tk.PhotoImage(file="GAME_IMAGES/altar/altar_adventure1.png")
+
+altar_adventure2_lucky = tk.PhotoImage(file="GAME_IMAGES/altar/altar_adventure2.png")
+altar_adventure2_curse_geyser = tk.PhotoImage(file="GAME_IMAGES/sanctuary/geyser_curse.png")
+altar_adventure2_curse_volcano = tk.PhotoImage(file="GAME_IMAGES/sanctuary/volcano_curse.png")
+
+altar_adventure3_lucky = tk.PhotoImage(file="GAME_IMAGES/altar/altar_adventure3.png")
+altar_adventure3_not_lucky = tk.PhotoImage(file="GAME_IMAGES/altar/altar_adventure3.png")
+
+altar_adventure4_lucky = tk.PhotoImage(file="GAME_IMAGES/altar/altar_adventure4.png")
+altar_adventure4_not_lucky = tk.PhotoImage(file="GAME_IMAGES/altar/altar_adventure4.png")
+
+altar_adventure5_lucky = tk.PhotoImage(file="GAME_IMAGES/altar/altar_adventure5.png")
+altar_adventure5_not_lucky = tk.PhotoImage(file="GAME_IMAGES/altar/altar_adventure5.png")
+
+
+#  PYRAMID ADVENTURE IMAGES
+pyramid_adventure1 = tk.PhotoImage(file="GAME_IMAGES/pyramid/pyramid_adventure1.png")
+
+pyramid_adventure2 = tk.PhotoImage(file="GAME_IMAGES/pyramid/pyramid_adventure2.png")
+
+pyramid_adventure3 = tk.PhotoImage(file="GAME_IMAGES/pyramid/pyramid_adventure3.png")
+
+pyramid_adventure4 = tk.PhotoImage(file="GAME_IMAGES/pyramid/pyramid_adventure4.png")
+
+pyramid_adventure5 = tk.PhotoImage(file="GAME_IMAGES/pyramid/pyramid_adventure5.png")
+
+dragon = tk.PhotoImage(file="GAME_IMAGES/pyramid/dragon.png")
+dragon_attack = tk.PhotoImage(file="GAME_IMAGES/pyramid/dragon_attack.png")
+sword = tk.PhotoImage(file="GAME_IMAGES/pyramid/sword.png")
+firebolt = tk.PhotoImage(file="GAME_IMAGES/pyramid/firebolt.png")
+
+
+help0 = tk.PhotoImage(file="GAME_IMAGES/help/help0.png")
+help1 = tk.PhotoImage(file="GAME_IMAGES/help/help1.png")
+help2 = tk.PhotoImage(file="GAME_IMAGES/help/help2.png")
+help3 = tk.PhotoImage(file="GAME_IMAGES/help/help3.png")
